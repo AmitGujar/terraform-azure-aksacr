d7bc951ad1939596cfdcc1397a201d1bfd6856c4
AmitGujar <amithero3342@gmail.com>
2023-09-22 14:46:05 +0530

    acr attached

--- aks_connect.sh
+++ aks_connect.sh
@@ -13,8 +13,12 @@ remove_config
 get_values() {
     aks_cluster_name=$(terraform output -raw aks_test)
     resource_group_name=$(terraform output -raw resource_group)
+    acr_name=$(terraform output -raw acr_name)
 
     echo "Getting credentials....."
     az aks get-credentials --resource-group $resource_group_name --name $aks_cluster_name
+
+    echo "Attaching ACR to the cluster....."
+    az aks update -n $aks_cluster_name -g $resource_group_name --attach-acr $acr_name
 }
 get_values
