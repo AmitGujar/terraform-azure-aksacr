a59725ae4a2f8729f9d875d5c768ecfe1786a365
AmitGujar <amithero3342@gmail.com>
2023-09-28 12:42:34 +0530

    commented acr attach

--- aks_connect.sh
+++ aks_connect.sh
@@ -3,7 +3,7 @@
 remove_config() {
     if [ -f ~/.kube/config ]; then
         rm ~/.kube/config
-        echo "Exiting config file is removed."
+        echo "Existing config file is removed."
     else
         echo "No config file found"
     fi
@@ -13,12 +13,12 @@ remove_config
 get_values() {
     aks_cluster_name=$(terraform output -raw aks_test)
     resource_group_name=$(terraform output -raw resource_group)
-    acr_name=$(terraform output -raw acr_name)
+    acr_name=$(terraform output -raw acr_test)
 
     echo "Getting credentials....."
     az aks get-credentials --resource-group $resource_group_name --name $aks_cluster_name
 
-    echo "Attaching ACR to the cluster....."
-    az aks update -n $aks_cluster_name -g $resource_group_name --attach-acr $acr_name
+#    echo "Attaching ACR to the cluster....."
+#   az aks update -n $aks_cluster_name -g $resource_group_name --attach-acr $acr_name
 }
 get_values
