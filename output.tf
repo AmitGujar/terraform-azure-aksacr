b44a0582b259ed8ef026a24d908364c5ee67cc10
AmitGujar <amithero3342@gmail.com>
2023-10-22 21:10:06 +0530

    output changed

--- output.tf
+++ output.tf
@@ -1,10 +1,10 @@
 output "resource_group" {
-  value = module.resource_group.resource_name
+  value       = module.resource_group.resource_name
   description = "Name of the resource group"
 }
 
 output "virtual_network" {
-  value = module.virtual_network.virtual_network
+  value       = module.virtual_network.virtual_network
   description = "Name of the virtual network"
 }
 
@@ -18,12 +18,12 @@ output "virtual_network" {
 #   value = module.virtual_machine.public_ip_address
 # }
 
-output "acr_test" {
-  value = module.acr_test.acr_test
+output "acr_name" {
+  value       = module.acr_test.acr_test
   description = "Name of the Azure Container Registry"
 }
 
-output "aks_test" {
-  value = module.aks_test.aks_test
+output "aks_name" {
+  value       = module.aks_test.aks_test
   description = "Name of the AKS cluster"
 }
