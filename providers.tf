b75436f027e95016b55d9bd5077703e55c671edb
AmitGujar <amithero3342@gmail.com>
2023-11-01 00:17:21 +0530

    files updated

--- providers.tf
+++ providers.tf
@@ -1,5 +1,14 @@
 terraform {
   required_version = ">= 1.0"
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "amithero3342"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "test"
+    }
+  }
 
   required_providers {
     azurerm = {
