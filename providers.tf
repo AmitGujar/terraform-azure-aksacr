b8b663567f437faaeced8b44debeba588e87876d
AmitGujar <amithero3342@gmail.com>
2023-11-01 01:15:50 +0530

    disabled remote backend

--- providers.tf
+++ providers.tf
@@ -1,14 +1,14 @@
 terraform {
   required_version = ">= 1.0"
-  backend "remote" {
-    # The name of your Terraform Cloud organization.
-    organization = "amithero3342"
+  # backend "remote" {
+  #   # The name of your Terraform Cloud organization.
+  #   organization = "amithero3342"
 
-    # The name of the Terraform Cloud workspace to store Terraform state files in.
-    workspaces {
-      name = "test"
-    }
-  }
+  #   # The name of the Terraform Cloud workspace to store Terraform state files in.
+  #   workspaces {
+  #     name = "test"
+  #   }
+  # }
 
   required_providers {
     azurerm = {
